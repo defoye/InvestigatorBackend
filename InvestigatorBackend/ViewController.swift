d94187f9e387ba00d664cbecffb7353f1d5dccb9
Ernest DeFoy III <erniedefoy3@gmail.com>
2019-07-06 13:35:56 -0400

    Initial Commit

---
+++ InvestigatorBackend/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InvestigatorBackend
+//
+//  Created by Ernest DeFoy on 7/6/19.
+//  Copyright © 2019 Ernest DeFoy III. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
